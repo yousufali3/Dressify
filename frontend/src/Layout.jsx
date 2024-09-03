a4cd8d1bfeff7312df07d1d5850ddba906df3279
Yousuf <yousufali06296@gmail.com>
2024-09-04 01:38:58 +0530

    fix footer

---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
