ab1344de151299ebf3eb1930402878788c34d02a
Yousuf <yousufali06296@gmail.com>
2024-09-05 20:21:50 +0530

    fixed issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
-  
->>>>>>> 42a0bbb85b856d872fff295ff6ce62fa800dc1bf
 });
