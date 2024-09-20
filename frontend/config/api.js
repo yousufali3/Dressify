01505f545aa64bf8972bd5f6c0366ffa6005c10c
Yousuf <yousufali06296@gmail.com>
2024-09-20 17:44:08 +0530

    a

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-const api = "https://dressify-backend.onrender.com/api/upload";
+const api =
+  "https://dressify-backend-fhy9nfjir-yousufali839203s-projects.vercel.app/api/upload";
 
 export default api;
